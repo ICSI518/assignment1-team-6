1f0d23321f592c5c5819226b7a52a7b963b191e8
kusuma469 <kusuma040620@gmail.com>
2024-09-14 15:06:41 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,12 +1,14 @@
+// Function to subtract two numbers
+function subtract(a, b) {
+  // return a - b;
+}
+
 // Function to add two numbers
 function add(a, b) {
   // return a + b;
 }
 
-// Function to subtract two numbers
-function subtract(a, b) {
-  // return a - b;
-}
+
 
 // Function to multiply two numbers
 function multiply(a, b) {

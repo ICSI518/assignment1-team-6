98a26a5acf3b2e45c4490c2dd4c5f9a7f3fd9f8d
Haritha Gampala <65433390+haritha568@users.noreply.github.com>
2024-09-14 20:23:16 -0400

    Update Activity1 red
    Update Activity1 red

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
+// Function to multiply two numbers
+function multiply(a, b) {
+  return a * b;
+}
+
 // Function to subtract two numbers
 function subtract(a, b) {
   return a - b;
@@ -7,11 +12,6 @@ function subtract(a, b) {
 function add(a, b) {
    return a + b;
 }
-// Function to multiply two numbers
-function multiply(a, b) {
-  return a * b;
-}
-
 
 // Function to divide two numbers
 function divide(a, b) {
